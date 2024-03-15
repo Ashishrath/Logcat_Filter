22129b3833c39fa6ca4218d22d11dee69ca0b82f
Ashish <ashishrath1997@gmail.com>
2024-03-15 22:16:39 +0530

    Basic working done

--- logcat/logcat/main.cpp
+++ logcat/logcat/main.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 
 enum tag { FATAL, ERROR, WARNING, INFO, DEBUG };
 enum filterOption {filterById, filterByTimeRange};
@@ -45,17 +47,22 @@ public:
         std::tm local_now;
         localtime_s(&local_now, &now_c);
         char buffer[80];
-        std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &local_now);
+        std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H_%M_%S", &local_now);
         return buffer;
     }
 };
 
 class LogFilter {
+private:
+    int pid;
+    int vid;
+    std::string startTime;
+    std::string endTime;
 public:
     LogFilter() {}
     LogFilter(int pid, int vid) : pid(pid), vid(vid) {}
-    //LogFilter(const std::string& startTime, const std::string& endTime)
-    //    : startTime(startTime), endTime(endTime) {}
+    LogFilter(const std::string& startTime, const std::string& endTime)
+        : startTime(startTime), endTime(endTime) {}
 
     bool filterById(const LogData& entry, int pid, int vid) const {
         if (entry.m_pid == pid && entry.m_vid == vid) {
@@ -65,79 +72,23 @@ public:
     }
 
     bool filterByTimeRange(const LogData& entry, std::string startTime, std::string endTime) const {
-        // Todo: time comparision
-        //if(entry.m_time >= startTime && entry.m_time <= endTime) {
-        //    return true;
-        //}
-        //return false;
+        std::string str = entry.m_time;
+        str.erase(std::remove(str.begin(), str.end(), ':'), str.end());
+        startTime.erase(std::remove(startTime.begin(), startTime.end(), ':'), startTime.end());
+        endTime.erase(std::remove(endTime.begin(), endTime.end(), ':'), endTime.end());
+       
+        return (std::stoi(str) >= std::stoi(startTime) && std::stoi(str) <= std::stoi(endTime));
     }
-
-private:
-    int pid;
-    int vid;
-    std::string startTime;
-    std::string endTime;
 };
 
 class LogParser {
 private:
     LogFilter filter;
-public:
-    std::vector<LogData> parseLogFile(const std::string& filename) {
-        std::vector<LogData> logEntries;
+    int pid, vid;
+    bool optionZeroIsSet = false, optionOneIsSet = false;
+    std::string startTime, endTime;
+    DateTime currTime;
 
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            std::cerr << "Error opening file: " << filename << std::endl;
-            return logEntries;
-        }
-
-        std::string line;
-        while (std::getline(file, line)) {
-            //std::cout << line << std::endl;
-            LogData entry = parseLogEntry(line);
-            logEntries.push_back(entry);
-        }
-
-        file.close();
-
-        int option;
-        std::cout << "Select filter option: " << std::endl;
-        std::cin >> option;
-
-        for (const auto& entry : logEntries) {
-            //std::cout << entry.m_date << std::endl;
-            if (option == 0) {
-                int pid, vid;
-                std::cout << "Enter pid: " << std::endl;
-                std::cin >> pid;
-                std::cout << "Enter vid: " << std::endl;
-                std::cin >> vid;
-                
-
-                if (filter.filterById(entry, pid, vid)) {
-                    // Output or process filtered log entries
-                    std::cout << entry.m_date << " " << entry.m_time << " " << entry.m_pid << " " << entry.m_vid << " " << "[" << entry.m_logLevel << "]" << /* entry.tag << */": " << entry.m_message << std::endl;
-                }
-            }
-            
-            if (option == 1) {
-                std::string startTime, endTime;
-                std::cout << "Enter startTime: " << std::endl;
-                std::cin >> startTime;
-                std::cout << "Enter endTime: " << std::endl;
-                std::cin >> endTime;
-                if (filter.filterByTimeRange(entry, startTime, endTime)) {
-                    std::cout << entry.m_date << " " << entry.m_time << " " << entry.m_pid << " " << entry.m_vid << " " << "[" << entry.m_logLevel << "]" << /* entry.tag << */": " << entry.m_message << std::endl;
-                }
-            }
-            
-        }
-
-        return logEntries;
-    }
-
-private:
     LogData parseLogEntry(const std::string& line) {
         std::string date = line.substr(0, 10);
         std::string time = line.substr(11, 8);
@@ -156,39 +107,111 @@ private:
         std::string level = line.substr(endPosOfVid + 1, endOfLevel - endPosOfVid - 1);
         std::string message = line.substr(endOfLevel + 1);
         LogData entry(date, time, pid, vid, level, message);
- 
+
         return entry;
+    }
+public:
+
+    void setPid(int p) {
+        pid = p;
+    }
+
+    void setVid(int v) {
+        vid = v;
+    }
+
+    void setOptionZeroIsSet(bool opt) {
+        optionZeroIsSet = opt;
+    }
+
+    void setOptionOneIsSet(bool opt) {
+        optionOneIsSet = opt;
+    }
+
+    void setStartTime(const std::string time) {
+        startTime = time;
+    }
+
+    void setEndTime(const std::string time) {
+        endTime = time;
+    }
+
+    std::vector<LogData> parseLogFile(const std::string& filename) {
+        std::vector<LogData> logEntries;
+
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file: " << filename << std::endl;
+            return logEntries;
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            LogData entry = parseLogEntry(line);
+            logEntries.push_back(entry);
+        }
+
+        file.close();
+
+        const std::string filteredLogFileName = "FilteredLogs/FilteredLog[" + currTime.getCurrentDateTime() + "].txt";
+        //std::cout << filteredLogFileName << std::endl;
+        std::ofstream logFile(filteredLogFileName);
+
+        if (!logFile.is_open()) {
+            std::cout << "Unable to open log file" << std::endl;
+        }
+
+        for (const auto& entry : logEntries) {
+            if (optionZeroIsSet == true && filter.filterById(entry, pid, vid)) {
+                logFile << entry.m_date << " " << entry.m_time << " " << entry.m_pid << " " << entry.m_vid << " " << "[" << entry.m_logLevel << "]" << /* entry.tag << */": " << entry.m_message << std::endl;
+            }
+
+            if (optionOneIsSet == true && filter.filterByTimeRange(entry, startTime, endTime)) {
+                logFile << entry.m_date << " " << entry.m_time << " " << entry.m_pid << " " << entry.m_vid << " " << "[" << entry.m_logLevel << "]" << /* entry.tag << */": " << entry.m_message << std::endl;
+            }      
+        }
+        logFile.close();
+        std::cout << "Logs have been written to file '"<< filteredLogFileName << "'" << std::endl;
+
+        return logEntries;
     }
 };
 
-/*class LogProcessor {
-public:
-    LogProcessor(const std::string& filename, int pid, int tid, const std::string& startTime, const std::string& endTime)
-     : filename(filename), filter(pid, tid, startTime, endTime) {}
-
-    void processLog() {
-        LogParser parser;
-        std::vector<LogData> logEntries = parser.parseLogFile(filename);
-
-        //for (const auto& entry : logEntries) {
-            //if (filter.filter(entry)) {
-                // Output or process filtered log entries
-                // std::cout << entry.getDate() << " " << entry.m_time << " " << static_cast<int>(entry.logLevel) << " " << entry.tag << ": " << entry.message << std::endl;
-            //}
-        //}
-    }
-
-private:
-    std::string filename;
-    LogFilter filter;
-};*/
-
-
-
-int main() {
-	std::string s;
-	//std::getline(std::cin, s);
+int main(int argc, char* argv[]) {
     DateTime d;
+    //std::cout << argc << std::endl;
+
+    //int option;
+    bool filterFlag = false;
+    bool levelFlag = false;
+
+    LogParser lp;
+
+    for (int i = 1; i < argc; i++) {
+        if (std::strcmp(argv[i], "-filter") == 0) {
+            i++;
+            filterFlag = true;
+            if (std::strcmp(argv[i], "0") == 0) {
+                //option = 0;
+                //lp.option = 0;
+                lp.setOptionZeroIsSet(true);
+                lp.setPid(std::stoi(argv[++i]));
+
+                lp.setVid(std::stoi(argv[++i]));
+            }
+            else if (std::strcmp(argv[i], "1") == 0) {
+                //lp.option = 1;
+                lp.setOptionOneIsSet(true);
+                lp.setStartTime(argv[++i]);
+
+                lp.setEndTime(argv[++i]);
+            }
+            else {
+                std::cout << "Invalid filter option" << std::endl;
+            }
+        }
+        //std::cout << argv[i] << std::endl;
+    }
 
     std::string date = (d.getCurrentDateTime()).substr(0, 10);
 	std::string time = (d.getCurrentDateTime()).substr(11);
@@ -199,7 +222,29 @@ int main() {
 	//tag t;
 	//LogData l(date, time, 1234, 11, "DEBUG", "Test Message");
 	//l.displayData();
-    LogParser lp;
+    
+    
+
+    
+    //std::cout << "Select filter option: " << std::endl;
+    //std::cin >> option;
+
+    /*if (filterFlag && option == 0) {
+        lp.option = 0;
+        std::cout << "Enter pid: " << std::endl;
+        std::cin >> lp.pid;
+        
+        std::cout << "Enter vid: " << std::endl;
+        std::cin >> lp.vid;
+    }
+    if (filterFlag && option == 1) {
+        lp.option = 1;
+        std::cout << "Enter startTime: " << std::endl;
+        std::cin >> lp.startTime;
+        std::cout << "Enter endTime: " << std::endl;
+        std::cin >> lp.endTime;
+    }*/
+
     lp.parseLogFile("logcat.txt");
 	return 0;
 }
